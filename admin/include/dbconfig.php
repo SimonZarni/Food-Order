4f46602886b70b203a6f8e59d698f6f56428f895
SimonZarni <simonallen364@gmail.com>
2024-03-24 22:45:08 +0630

    Structure template

---
+++ admin/include/dbconfig.php
@@ -0,0 +1,24 @@
+<?php
+
+class Database {
+
+    private static $hostname = 'localhost';
+    private static $username = 'root';
+    private static $password = '';
+    private static $dbname = 'food_order';
+    private static $connection = null;
+
+    public static function connect()
+    {
+        if(!self::$connection)
+            self::$connection = new PDO("mysql:host=".self::$hostname.";dbname=".self::$dbname, self::$username, self::$password);
+        return self::$connection;
+    }
+
+    public function disconnect()
+    {
+        self::$connection = null;
+    }
+}
+
+?>
---
+++ admin/views/township/township_list.php
@@ -0,0 +1,7 @@
+<?php
+include_once __DIR__ . '/../../layouts/sidebar.php';
+?>
+
+<?php
+include_once __DIR__ . '/../../layouts/footer.php';
+?>

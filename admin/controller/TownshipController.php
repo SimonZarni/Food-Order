ddd9aef54f0dbac2b4e4c20dceb0e540563d87d4
naymyokanaung <151381557+naymyokanaung@users.noreply.github.com>
2024-03-29 15:37:48 +0630

    Add files via upload

--- admin/controller/TownshipController.php
+++ admin/controller/TownshipController.php
@@ -1,1 +1,32 @@
-h
+<?php
+include_once __DIR__ . '/../model/Township.php';
+
+class TownshipController{
+    private $township;
+    function __construct(){
+        $this->township=new township();
+    }
+
+    public function getTownships(){
+        return $this->township->getTownships();
+    }
+
+    public function getTownship($id){
+        return $this->township->getTownship($id);
+    }
+
+    public function addTownship($township, $fee){
+        return $this->township->addTownship($township, $fee);
+    }
+
+    public function updateTownship($id,$township, $fee){
+        return $this->township->updateTownship($id,$township, $fee);
+    }
+
+    public function deleteTownship($id){
+        return $this->township->deleteTownship($id);
+    }
+}
+
+?>
+

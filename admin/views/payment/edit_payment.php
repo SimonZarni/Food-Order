acb3bfdec68f8d953e4864e572abc25cde814a87
naymyokanaung <naymyokanaung@gmail.com>
2024-03-30 17:14:36 +0630

    second commit

--- admin/views/payment/edit_payment.php
+++ admin/views/payment/edit_payment.php
@@ -1,7 +1,4 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 session_start();
 include_once __DIR__ . "/../../controller/PaymentController.php";
 $id=$_GET['id'];
--- admin/views/township/delete_township.php
+++ admin/views/township/delete_township.php
@@ -1,6 +1,4 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 include_once __DIR__ . "/../../controller/TownshipController.php";
 $id=$_GET['id'];
 
